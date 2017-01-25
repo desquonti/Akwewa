ac2d1cb99081d06ae01d646668886645ffd7fa40
DESQUONTI KAN <DESQUONTI KAN>
2017-01-25 14:26:44 +0100

    Add bootstrap and material design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcom/index'
+  get 'welcom/sign'
+
   devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index'
+
 end
